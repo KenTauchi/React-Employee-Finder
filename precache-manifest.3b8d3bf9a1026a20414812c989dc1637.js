9726ca3f775db16be8c2c9525f58c4290443c842
Ken Tauchi <kentauchica@gmail.com>
2020-08-26 18:06:59 -0700

    Updates

---
+++ precache-manifest.3b8d3bf9a1026a20414812c989dc1637.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "22314b9f4e7712d95c9820945824c739",
+    "url": "/KenTauchi/React-Employee-Finder.git/index.html"
+  },
+  {
+    "revision": "24cdae902a6b5af20e2b",
+    "url": "/KenTauchi/React-Employee-Finder.git/static/css/main.4788d745.chunk.css"
+  },
+  {
+    "revision": "82dde2114812aa4bc4f1",
+    "url": "/KenTauchi/React-Employee-Finder.git/static/js/2.18c5bf04.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/KenTauchi/React-Employee-Finder.git/static/js/2.18c5bf04.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "24cdae902a6b5af20e2b",
+    "url": "/KenTauchi/React-Employee-Finder.git/static/js/main.420cc720.chunk.js"
+  },
+  {
+    "revision": "721af5bd277c9d700988",
+    "url": "/KenTauchi/React-Employee-Finder.git/static/js/runtime-main.efd8ea35.js"
+  },
+  {
+    "revision": "5d5d9eefa31e5e13a6610d9fa7a283bb",
+    "url": "/KenTauchi/React-Employee-Finder.git/static/media/logo.5d5d9eef.svg"
+  }
+]);
